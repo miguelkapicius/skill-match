3f19d903da749f1dda525801c29eb8edb3da4a70
Miguel Kapicius <miguelkapicius@gmail.com>
2025-11-11 12:49:45 -0300

    first commit

--- api/src/env.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const envSchema = z.object({
-  GEMINI_API_KEY: z.string().min(1, { message: "GEMINI_API_KEY is required" }),
-});
-
-const env = envSchema.parse(process.env);
-
-export default env;
